e5e13f83ced997a488fcc853bc7d933ecf024c01
AlesyaSuperfin <superfin.alesya@gmail.com>
2024-04-29 17:07:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+
+  const [places, setPlaces] = useState(data);
+  const [element, setElement] = useState(0);
+  const {id, name, country, image, source, description} = data[element];
+
+  const previousElement = () => {
+    setElement((element => {
+      element --;
+      if(element < 0) {
+        return data.length -1;
+      }
+      return element;
+    }))
+
+  }
+
+  const nextElement = () => {
+    setElement((element => {
+      element ++;
+      if (element > data.length -1) {
+        element =0;
+      }
+      return element;
+    }))
+  }
+
+  const removeElement = (id) => {
+    let newPlaces = places.filter(place => place.id !== id);
+    setPlaces (newPlaces)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+    <div className="header">
+      <h1>TOP-15 Must See Places in The World</h1>
+      <p>There are never-ending reasons to travel, but many go to seek those awe-inspiring moments that stop them in their tracks, searching for the sights that will stay with them forever. Every day, travelers find those experiences among the ruins of Machu Picchu or at the summit of Mount Kilimanjaro.</p>
+      <p>From sky-blue lakes in Canada to sandstone beaches in Seychelles, beauty abounds in every corner of the Earth. Focusing largely on national parks, mountains, beaches, deserts, and other natural wonders, we’ve compiled the world's most beautiful places to inspire your next dream destination.</p>
+      <button onClick={() => setPlaces([])}>I've already explored all of them!</button>
+      <h2 className='resultText'>You've explored {15 -places.length} of 15 the most beautiful places in the world!</h2>
+      <h3>It's time to plan your next journey!</h3>
     </div>
-  );
+
+      {/* {places.map((element => {
+        const{id, name, country, image, source, description} = element;
+        
+
+        return ( */}
+          <div className='slider'>
+
+          <div>
+          <img src={image} alt="place" width="100%"/>
+          <p className='textPhoto'>PHOTO: {source}</p>
+          <h2>{name}</h2>
+          <h3>⚲ {country}</h3>
+          <p>{description}</p>
+          </div>
+          
+          <div className='btn'>
+            <button onClick={previousElement}>Previous</button>
+            <button onClick={() => removeElement(id)}>I've explored this place!</button>
+            <button onClick={nextElement}>Next</button>
+          </div>
+          </div>
+        )
+
+      }))}
+
+
+      </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,155 @@
+import imageOne from './1.jpg';
+import imageTwo from './2.jpg';
+import imageThree from './3.jpg';
+import imageFour from './4.jpg';
+import imageFive from './5.jpg';
+import imageSix from './6.jpg';
+import imageSeven from './7.jpg';
+import imageEight from './8.jpg';
+import imageNine from './9.jpg';
+import imageTen from './10.jpg';
+import imageEleven from './11.jpg';
+import imageTwelve from './12.jpg';
+import imageThirteen from './13.jpg';
+import imageForteen from './14.jpg';
+import imageFifteen from './15.jpg';
+
+export const data = [
+
+    {
+        id: 1,
+        name: "Torres del Paine National Park",
+        country: "Chile",
+        image: imageOne,
+        source: "Davidionut, Adobe Stock",
+        description: "There's no better way to experience Patagonia's rugged natural beauty than in Torres del Paine National Park. The UNESCO Biosphere Reserve is home to its namesake granite towers (the park's name is an amalgam of the Spanish word for “towers” and the Tehuelche word for “blue”), as well as sparkling lagoons and otherworldly glaciers.",
+    },
+
+    {
+        id: 2,
+        name: "Victoria Falls",
+        country: "Zimbabwe and Zambia",
+        image: imageTwo,
+        source: "https://www.istockphoto.com/",
+        description: "One of the seven natural wonders of the world, Victoria Falls spans the Zimbabwe and Zambia borders. `The smoke that thunders,` as it is known to locals, and its surroundings, are hubs for white-water rafting, helicopter rides, big-game safaris, and other thrilling adventures.",
+    },
+
+    {
+        id: 3,
+        name: "Hạ Long Bay",
+        country: "Vietnam",
+        image: imageThree,
+        source: "https://jptraveltime.com/",
+        description: "With hundreds of jungle-covered karsts springing out of emerald green waters, Hạ Long Bay is a photographer's dream. Hop on a boat or kayak to explore the UNESCO World Heritage Site's beautiful islands and surreal cave systems.",
+    },
+
+    
+    {
+        id: 4,
+        name: "Dead Sea",
+        country: "Israel and Jordan",
+        source: "https://www.touristisrael.com/",
+        image: imageFour,
+        description: "Name aside, the Dead Sea is a lake and one of the world's saltiest, at that (in fact, the saline water has such a high density that it keeps bathers afloat). Set between Jordan and Israel at the Earth's lowest elevation, it's known for its beautiful clear and tranquil waters and surrounding mineral formations, sandy beaches, nature preserves, natural pools, and waterfalls.",
+    },
+
+    {
+        id: 5,
+        name: "Machu Picchu",
+        country: "Peru",
+        image: imageFive,
+        source: "https://www.keadventure.com/",
+        description: "Built nearly 8,000 feet above sea level, Machu Picchu is a 15th-century Incan citadel whose engineering ingenuity and head-spinning views rival the extraordinary beauty of its Sacred Valley setting.",
+    },
+
+    {
+        id: 6,
+        name: "The Algarve",
+        country: "Portugal",
+        image: imageSix,
+        source: "GONZALO AZUMENDI/GETTY IMAGES",
+        description: "Surrounded by the Atlantic to the south and west, Portugal's Algarve region looks and feels like a wild respite. Wind-sculpted cliffs and headlands frame big, sandy beaches popular with surfers and dotted with secret coves and grottoes.",
+    },
+
+    {
+        id: 7,
+        name: "Mount Fuji",
+        country: "Japan",
+        image: imageSeven,
+        source: "DOCTOREGG/GETTY IMAGES",
+        description: "Both an active volcano and an icon of serenity, Mount Fuji is one of the world's most magnificent sights. You can hike to the sacred landmark's summit for sweeping views or simply gape at it from Lake Kawaguchi and elsewhere in the beautiful Five Lakes region. Head to the spa town of Hakone in winter to pair crisp Fuji views with steaming hot springs.",
+    },
+
+    {
+        id: 8,
+        name: "Lofoten Islands",
+        country: "Norway",
+        image: imageEight,
+        source: "DENNIS FISCHER PHOTOGRAPHY/GETTY IMAGES",
+        description: "The Lofoten archipelago bursts dramatically out of the Norwegian Sea. Between the jagged peaks and steep slopes are quaint fishing villages, secluded coves, scenic backpacking and biking trails, and dreamy white-sand beaches.",
+    },
+
+    {
+        id: 9,
+        name: "Banff National Park",
+        country: "Canada",
+        image: imageNine,
+        source: "Jeff Penner/EyeEm/Getty Images",
+        description: "Canada's oldest national park showcases the majesty of the Canadian Rockies in Alberta. Banff is known for its staggering peaks, dense pine forests, hot springs, animals (grizzlies, bighorn sheep, and moose all call the park home), and almost eerily azure glacier-fed lakes, such as the postcard-perfect Peyto Lake.",
+    },
+
+    {
+        id: 10,
+        name: "Milford Sound/Piopiotahi",
+        country: "New Zealand",
+        image: imageTen,
+        source: "https://www.locationscout.net/Donovan Callaghan",
+        description: "Glaciers carved this awe-inspiring valley, leaving behind craggy peaks with sheer drops into glistening waters. Part of UNESCO World Heritage Site Te Wahipounamu, Milford Sound is blessed with waterfalls, rainbows, rainforest, and a diversity of wildlife — everything from black coral to bottlenose dolphins, with seals and even penguins in between.",
+    },
+
+    {
+        id: 11,
+        name: "Mount Kilimanjaro",
+        country: "Tanzania",
+        image: imageEleven,
+        source: "https://indiahikes.com/",
+        description: "Solitary Mount Kilimanjaro is one of Africa's most stunning icons, rising more than 19,000 feet from coffee and banana farms up to snowy volcanic peaks. Along the way, hikers trek through distinct climate zones, from rainforest to high-altitude desert to the arctic-like summit (aka `the roof of Africa`).",
+    },
+
+    {
+        id: 12,
+        name: "Great Barrier Reef",
+        country: "Australia",
+        image: imageTwelve,
+        source: "COLIN BAKER/GETTY IMAGES",
+        description: "The world's largest barrier reef is an unrivaled experience for snorkelers and divers. The sensational underwater world is made up of 2,900 individual reefs, 900 islands, and a mind-boggling diversity of marine life.",
+    },
+
+    {
+        id: 13,
+        name: "Pamukkale",
+        country: "Turkey",
+        image: imageThirteen,
+        source: "https://www.luxsphere.co/",
+        description: "Pamukkale's electric-blue thermal pools and white travertine formations, naturally formed by slowly crystallizing calcium carbonate, make for gorgeous vacation photos.",
+    },
+
+    {
+        id: 14,
+        name: "Avenue of the Baobabs",
+        country: "Madagascar",
+        image: imageForteen,
+        source: "https://www.worldatlas.com/",
+        description: "On a dirt road near Madagascar's west coast is a stretch of enormous baobab trees, all that remains of a once-dense forest. The centuries-old giants are especially magnificent cast in the light of a sunrise and sunset.",
+    },
+
+    {
+        id: 15,
+        name: "Anse Source d'Argent",
+        country: "Seychelles",
+        image: imageFifteen,
+        source: "https://www.jonnymelon.com/",
+        description: "Pinpointing the most beautiful Seychellois beach is like splitting hairs, but Anse Source d'Argent gets extremely high marks for its sugar-white sand framed by dramatic granite boulders and sparkling aquamarine water. The sweet shoreline is relatively secluded from the resort crowds on La Digue Island.",
+    },
+]
+
